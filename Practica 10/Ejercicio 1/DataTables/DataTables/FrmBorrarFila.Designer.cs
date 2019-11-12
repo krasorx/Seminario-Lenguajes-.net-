1b63644d831c9a8df9f24958334afd0d8d846f28
Luis <40400309+krasorx@users.noreply.github.com>
2019-11-11 23:21:19 -0300

    Add files via upload
    Practica 10, ejercicio 1 finalizado

---
+++ Practica 10/Ejercicio 1/DataTables/DataTables/FrmBorrarFila.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DataTables
+{
+    partial class FrmBorrarFila
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbFilas = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(30, 5, 30, 5);
+            this.label1.Size = new System.Drawing.Size(414, 114);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Indique el número de fila que desea eliminar de la tabla";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbFilas
+            // 
+            this.cmbFilas.BackColor = System.Drawing.Color.DimGray;
+            this.cmbFilas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbFilas.ForeColor = System.Drawing.Color.Red;
+            this.cmbFilas.FormattingEnabled = true;
+            this.cmbFilas.Location = new System.Drawing.Point(187, 105);
+            this.cmbFilas.Name = "cmbFilas";
+            this.cmbFilas.Size = new System.Drawing.Size(80, 32);
+            this.cmbFilas.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.MintCream;
+            this.btnAceptar.Location = new System.Drawing.Point(16, 174);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCancelar.Location = new System.Drawing.Point(351, 174);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FrmBorrarFila
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(438, 209);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cmbFilas);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBorrarFila";
+            this.Text = "FrmBorrarFila";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFilas;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ Practica 10/Ejercicio 1/DataTables/DataTables/FrmBorrarFila.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataTables
+{
+    public partial class FrmBorrarFila : Form
+    {
+        private int rowSize;
+        private FrmMain main;
+        private ArrayList rows = new ArrayList();
+
+        public FrmBorrarFila(ref int size,FrmMain frmMain)
+        {
+            rowSize = size;
+            main = frmMain;
+            InitializeComponent();
+
+            LoadRowList();
+
+        }
+
+        private void LoadRowList()
+        {
+            for (int i = 0; i < rowSize; i++)
+            {
+                rows.Add("Fila " + i);
+            }
+
+            cmbFilas.DataSource = rows;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            main.SetIndex(-1);
+            btnCancelar.DialogResult = DialogResult.Cancel;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            main.SetIndex(cmbFilas.SelectedIndex);
+            btnAceptar.DialogResult = DialogResult.OK;
+        }
+    }
+}
